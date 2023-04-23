e7bd7de09e9dbd3906a722832270b35c77f4a66b
militist <neumpanin@yandex.ru>
2023-04-23 15:52:11 +0300

    добавлен фаил pies

---
+++ pies.py
@@ -0,0 +1,8 @@
+from bakery_products import Bakery_products
+
+class Pies(Bakery_products):
+    def __init__(self, price, expiration_Date):
+        super().__init__(price, expiration_Date)
+
+    def getInfo(self):
+        print(f"Товар имеет следующие цену: {price} и срок годности: {expiration_Date}.")
